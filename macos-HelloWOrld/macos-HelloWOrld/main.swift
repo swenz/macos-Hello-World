57af68ae0e508fec68b747fc8f04032314dc4a52
Steffen Wenz <steffen@wenz.io>
2017-03-27 14:16:02 +0200

    no message

---
+++ macos-HelloWOrld/macos-HelloWOrld/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  macos-HelloWOrld
+//
+//  Created by Steffen Wenz on 27/03/2017.
+//  Copyright © 2017 Steffen Wenz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
